72794fec10d388df729b845c49c9498b7f4ddbf8
Nolan Cutler <nbcutler@smu.edu>
2025-12-15 21:05:38 -0600

    Fix base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change to '/repo-name/' if not using username.github.io
+  base: '/nbcutler/', // Change to '/repo-name/' if not using username.github.io
 })
