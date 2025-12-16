69b4aeb515a67bdeab2e85d53a6c141cbe90a830
Nolan Cutler <nbcutler@smu.edu>
2025-12-15 21:52:04 -0600

    Directory name change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nbcutler/', // Change to '/repo-name/' if not using username.github.io
+  base: '/portfolio/', // Change to '/repo-name/' if not using username.github.io
 })
